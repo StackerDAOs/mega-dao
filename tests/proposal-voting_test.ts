8f8c120840e074139179ccb3ed2f9e7f1bd9a6b4
Ryan <ryan.waits@gmail.com>
2022-07-28 17:51:30 -0500

    add tests for delegate vote and concluding proposal

--- tests/proposal-voting_test.ts
+++ tests/proposal-voting_test.ts
@@ -220,19 +220,128 @@ Clarinet.test({
 Clarinet.test({
   name: 'Vote as a delegate',
   async fn(chain: Chain, accounts: Map<string, Account>) {
-    // TODO: test case for voting as a delegate
-  },
-});
-
-Clarinet.test({
-  name: 'Conclude and execute proposal',
-  async fn(chain: Chain, accounts: Map<string, Account>) {
-    // TODO
+    const deployer = accounts.get('deployer')!;
+    const { init } = daoApi(chain, deployer);
+    const { receipts } = chain.mineBlock([
+      init(BOOTSTRAPS.MEGA_DAO),
+    ])
+    receipts[0].result.expectOk().expectBool(true);
+    const { propose } = submissionApi(chain, deployer);
+    const validStartHeight = chain.blockHeight + 144;
+    const { receipts: proposalReceipts } = chain.mineBlock([
+      propose(
+        PROPOSALS.TRANSFER_TOKENS,
+        validStartHeight,
+      ),
+    ]);
+    proposalReceipts[0].result.expectOk().expectBool(true);
+    const delegateWallet = accounts.get('wallet_1')!;
+    const { delegate } = votingApi(chain, deployer);
+    const { receipts: delegateReceipts } = chain.mineBlock([
+      delegate(delegateWallet.address)
+    ]);
+    delegateReceipts[0].result.expectOk().expectBool(true);
+    chain.mineEmptyBlockUntil(147);
+    const { vote, getProposalData } = votingApi(chain, delegateWallet);
+    const { receipts: voteReceipts } = chain.mineBlock([
+      vote(
+        true,
+        PROPOSALS.TRANSFER_TOKENS,
+        deployer.address,
+      ),
+    ]);
+    voteReceipts[0].result.expectOk().expectBool(true);
+    const proposal = getProposalData(PROPOSALS.TRANSFER_TOKENS).result;
+    const event = voteReceipts[0].events[0].contract_event?.value
+    assertEquals(event.expectTuple(), {
+      amount: types.uint(100),
+      delegate: types.some(delegateWallet.address),
+      event: types.ascii('vote'),
+      for: types.bool(true),
+      proposal: PROPOSALS.TRANSFER_TOKENS,
+      voter: deployer.address,
+    })
+    assertEquals(proposal.expectSome().expectTuple(), {
+      votesFor: types.uint(100),
+      votesAgainst: types.uint(0),
+      startBlockHeight: types.uint(validStartHeight),
+      endBlockHeight: types.uint(866),
+      concluded: types.bool(false),
+      passed: types.bool(false),
+      proposer: deployer.address,
+    });
   },
 });
 
 Clarinet.test({
   name: 'Conclude and reject proposal',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get('deployer')!;
+    const anotherVoter = accounts.get('wallet_1')!;
+    const { init } = daoApi(chain, deployer);
+    const { receipts } = chain.mineBlock([
+      init(BOOTSTRAPS.MEGA_DAO),
+    ])
+    receipts[0].result.expectOk().expectBool(true);
+    const { propose } = submissionApi(chain, deployer);
+    const validStartHeight = chain.blockHeight + 144;
+    const { receipts: proposalReceipts } = chain.mineBlock([
+      propose(
+        PROPOSALS.TRANSFER_TOKENS,
+        validStartHeight,
+      ),
+    ]);
+    proposalReceipts[0].result.expectOk().expectBool(true);
+    chain.mineEmptyBlockUntil(147);
+    const { vote, getProposalData } = votingApi(chain, deployer);
+    const { receipts: voteReceipts } = chain.mineBlock([
+      vote(
+        true,
+        PROPOSALS.TRANSFER_TOKENS,
+        undefined,
+      ),
+    ]);
+    voteReceipts[0].result.expectOk().expectBool(true);
+    const { vote: anotherVote } = votingApi(chain, anotherVoter);
+    const { receipts: anotherVoteReceipts } = chain.mineBlock([
+      anotherVote(
+        true,
+        PROPOSALS.TRANSFER_TOKENS,
+        undefined,
+      ),
+    ]);
+    anotherVoteReceipts[0].result.expectOk().expectBool(true);
+    assertEquals(getProposalData(PROPOSALS.TRANSFER_TOKENS).result.expectSome().expectTuple(), {
+      votesFor: types.uint(15100),
+      votesAgainst: types.uint(0),
+      startBlockHeight: types.uint(validStartHeight),
+      endBlockHeight: types.uint(866),
+      concluded: types.bool(false),
+      passed: types.bool(false),
+      proposer: deployer.address,
+    });
+    chain.mineEmptyBlockUntil(1110);
+    const { conclude } = votingApi(chain, deployer);
+    const { receipts: concludeReceipts } = chain.mineBlock([
+      conclude(
+        PROPOSALS.TRANSFER_TOKENS,
+      ),
+    ]);
+    concludeReceipts[0].result.expectOk().expectBool(false);
+    assertEquals(getProposalData(PROPOSALS.TRANSFER_TOKENS).result.expectSome().expectTuple(), {
+      votesFor: types.uint(15100),
+      votesAgainst: types.uint(0),
+      startBlockHeight: types.uint(validStartHeight),
+      endBlockHeight: types.uint(866),
+      concluded: types.bool(true),
+      passed: types.bool(false),
+      proposer: deployer.address,
+    });
+  },
+});
+
+Clarinet.test({
+  name: 'Conclude and execute proposal',
   async fn(chain: Chain, accounts: Map<string, Account>) {
     // TODO
   },
